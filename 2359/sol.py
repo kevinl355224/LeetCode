c06e0bdacd601d5b9bf213f9e7aea4cd68ba32fc
kevinl355224 <134241836+kevinl355224@users.noreply.github.com>
2025-05-30 20:44:22 +0800

    Add 2359: sol.py

---
+++ 2359/sol.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        
+
+
+if __name__ == "__main__":
+    edges = [2,2,3,-1]
+    node1 = 0
+    node2 = 1
+    sol = Solution()
+    print(sol.closestMeetingNode(edges, node1, node2))
