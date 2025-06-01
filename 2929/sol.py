4ca3c71df6a98eda3660f2e98042149c60a2bc0b
kevinl355224 <134241836+kevinl355224@users.noreply.github.com>
2025-06-01 21:00:52 +0800

    Add 2929: sol.py
    Init

---
+++ 2929/sol.py
@@ -0,0 +1,9 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        pass
+        
+if __name__ == "__main__":
+    sol = Solution()
+    n = 5
+    limit = 2
+    print(sol.distributeCandies(n, limit))
