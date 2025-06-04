3631d55ebf7a92a6ee04751865e2ddaf988c1a82
Kelvin <kevinl355224@gmail.com>
2025-06-04 15:07:52 +0800

    Add 3403: sol.py

---
+++ 3403/sol.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    word = "dbca"
+    numFriends = 2
+    print(sol.answerString(word, numFriends))
