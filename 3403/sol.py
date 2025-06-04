0316939d19b6f0053fe6f3f9ff79fb4c5f81a5cc
Kelvin <kevinl355224@gmail.com>
2025-06-04 17:52:06 +0800

    Update 3403: sol.py

--- 3403/sol.py
+++ 3403/sol.py
@@ -2,6 +2,20 @@ from itertools import combinations
 
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+
+        n = len(word)
+        length = n - numFriends + 1
+        max_char = max(word)
+        result = ""
+
+        for i in range(n):
+            if word[i] == max_char:
+                substr = word[i:i + length]
+                result = max(result, substr)
+
+        return result
 
 
 if __name__ == "__main__":
