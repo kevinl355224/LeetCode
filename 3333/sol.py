c8f9f1a3a8a1440c4f93df3dfb5436f2e8591b2d
Kelvin <kevinl355224@gmail.com>
2025-07-02 14:20:40 +0800

    Update 3333: sol.py

--- 3333/sol.py
+++ 3333/sol.py
@@ -11,9 +11,58 @@ class Solution:
         """
         MOD = 10**9 + 7
 
-        
+        # Break down words into groups num of word etc. "abbccc" -> [1, 2, 3]
+        groups = []
+        count = 1
+        for i in range(1, len(word)):
+            if word[i] == word[i - 1]:
+                count += 1
+            else:
+                groups.append(count)
+                count = 1
+        groups.append(count)
+
+        # All possible combination without consider k.
+        total = 1
+        for num in groups:
+            total = (total * num) % MOD
+
+        if k < len(groups):
+            return total
+
+        # Init dp
+        # dp[s] means: the number of ways to keep exactly s characters in the original string.
+        # dp = [1,0,0,0...]
+        dp = [0] * k
+        dp[0] = 1
+        """
+        ex: word = "aabbccdd"
+        groups = [2, 2, 2, 2]
+
+        At this point, dp[s] represents the number of ways to keep exactly s characters so far.
+
+        When processing a group of length 2 (e.g., the 'c' group),
+        to keep exactly 2 characters, we sum dp[s - x] for x in [1..2],
+        meaning we add the ways to keep (2-1)=1 or (2-2)=0 characters from previous groups,
+        and keep x characters from the current group,
+        forming subsequences of length 2 in total.
+        """
+        for num in groups:
+            new_dp = [0] * k
+            sum_value = 0
+            for i in range(1, k):
+                # Sliding window
+                sum_value = (sum_value + dp [i - 1]) % MOD
+                if i > num:
+                    sum_value = (sum_value - dp[i - num - 1] + MOD) % MOD
+                new_dp[i] = sum_value
+            dp = new_dp
+
+        invalid = sum(dp[len(groups):k]) % MOD
+        return (total - invalid + MOD) % MOD
+    
 if __name__ == "__main__":
     sol = Solution()
-    word = "aaaabbbb"
-    k = 6
+    word = "aabbccdd"
+    k = 7
     print(sol.possibleStringCount(word, k))
