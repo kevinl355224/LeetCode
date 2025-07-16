572ea4e8306af3086ed97d500e937f1afc1f37a0
Kelvin <kevinl355224@gmail.com>
2025-07-16 14:15:31 +0800

    Update 3201: revise sol.py and add sol2.py

--- 3201/sol2.py
+++ 3201/sol2.py
@@ -11,19 +11,23 @@ class Solution:
         
         Return the length of the longest valid subsequence of nums.
         """
-        odd_cnt = 0
-        convert_lenght = 0
-        pre = None
-
-        for i in range(len(nums)):
-            is_odd = nums[i] & 1
-            if is_odd:
-                odd_cnt += 1
-            if pre != is_odd:
-                convert_lenght += 1
-                pre = is_odd
-
-        return max(len(nums) - odd_cnt, odd_cnt, convert_lenght)
+        count_even = 0
+        count_odd = 0
+        for num in nums:
+            if num % 2 == 0:
+                count_even += 1
+            else:
+                count_odd += 1
+        
+        even_dp = 0
+        odd_dp = 0
+        for num in nums:
+            if num % 2 == 0:
+                even_dp = max(even_dp, odd_dp + 1)
+            else:
+                odd_dp = max(odd_dp, even_dp + 1)
+        
+        return max(count_even, count_odd, even_dp, odd_dp)
 
 if __name__ == "__main__":
     sol = Solution()
