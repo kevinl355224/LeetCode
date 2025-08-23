05db5c781dd7225a4ad2c218a8977bdf38618bf9
Kelvin <kevinl355224@gmail.com>
2025-08-23 23:04:35 +0800

    Add 3197: init

---
+++ 3197/sol.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def minimumSum(self, grid: List[List[int]]) -> int:
+        
+
+if __name__ == "__main__":
+    sol = Solution()
+    grid = [[1,0,1],[1,1,1]]
+    print(sol.minimumSum(grid))
